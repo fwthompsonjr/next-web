5935b97a06b053e2dbd050ea9996548a443189d2
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2024-08-20 18:09:27 -0500

    test: ensure proper fetch of destination urls

--- src/next.processor.api.tests/utility/PostAddressProviderTests.cs
+++ src/next.processor.api.tests/utility/PostAddressProviderTests.cs
@@ -9,6 +9,7 @@ namespace next.processor.api.tests.utility
         {
             var actual = PostAddressProvider.BaseApiAddress();
             Assert.False(string.IsNullOrEmpty(actual));
+            Assert.True(Uri.IsWellFormedUriString(actual, UriKind.Absolute));
         }
 
         [Fact]
@@ -17,6 +18,21 @@ namespace next.processor.api.tests.utility
             var actual = PostAddressProvider.PostAddresses();
             Assert.NotNull(actual);
             Assert.NotEmpty(actual);
+        }
+
+        [Theory]
+        [InlineData("initialize")]
+        [InlineData("update")]
+        [InlineData("fetch")]
+        [InlineData("start")]
+        [InlineData("status")]
+        [InlineData("finalize")]
+        public void CollectionContainsAddress(string prefix)
+        {
+            var list = PostAddressProvider.PostAddresses();
+            var actual = list?.Find(x => x.Name == prefix);
+            Assert.NotNull(actual);
+            Assert.True(Uri.IsWellFormedUriString(actual.Address, UriKind.Absolute));
         }
     }
 }
--- src/next.processor.api/utility/PostAddressProvider.cs
+++ src/next.processor.api/utility/PostAddressProvider.cs
@@ -6,44 +6,58 @@ namespace next.processor.api.utility
     {
         public static string? BaseApiAddress()
         {
-            const char comma = ',';
-            const string source = "api.permissions:{0}";
-            var keys = "destination,remote,local".Split(comma).ToList();
-            var values = new List<string?>();
-            var config = SettingsProvider.Configuration;
-            keys.ForEach(key =>
+            lock (locker)
             {
-                var setting = string.Format(source, key);
-                var value = config[setting];
-                values.Add(value);
-            });
-            var destination = values[0];
-            if (string.IsNullOrEmpty(destination)) return null;
-            var id = destination.Equals("local") ? 1 : 0;
-            return values[id];
+                if (!string.IsNullOrWhiteSpace(_baseapi)) return _baseapi;
+                const char comma = ',';
+                const string source = "api.permissions:{0}";
+                var keys = "destination,remote,local".Split(comma).ToList();
+                var values = new List<string?>();
+                var config = SettingsProvider.Configuration;
+                keys.ForEach(key =>
+                {
+                    var setting = string.Format(source, key);
+                    var value = config[setting];
+                    values.Add(value);
+                });
+                var destination = values[0];
+                if (string.IsNullOrEmpty(destination)) return null;
+                var id = destination.Equals("local") ? 1 : 2;
+                _baseapi = values[id];
+                return _baseapi; 
+            }
         }
 
 
         public static List<ApiAddress>? PostAddresses()
         {
-            const char comma = ',';
-            const string source = "post_address:{0}";
-            var baseapi = BaseApiAddress();
-            if (string.IsNullOrEmpty(baseapi)) return null;
-            var keys = "initialize,update,fetch,start,status,finalize".Split(comma).ToList();
-            var values = new List<ApiAddress>();
-            var config = SettingsProvider.Configuration;
-            keys.ForEach(key =>
+            lock (locker)
             {
-                var setting = string.Format(source, key);
-                var value = config[setting];
-                if (!string.IsNullOrEmpty(value))
+                if (_postaddresses != null) return _postaddresses;
+                const char comma = ',';
+                const string source = "post_address:{0}";
+                var baseapi = BaseApiAddress();
+                if (string.IsNullOrEmpty(baseapi)) return null;
+                var keys = "initialize,update,fetch,start,status,finalize".Split(comma).ToList();
+                var values = new List<ApiAddress>();
+                var config = SettingsProvider.Configuration;
+                keys.ForEach(key =>
                 {
-                    var address = $"{baseapi}{value}";
-                    values.Add(new() { Name = key, Address = address });
-                }
-            });
-            return values;
+                    var setting = string.Format(source, key);
+                    var value = config[setting];
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        var address = $"{baseapi}{value}";
+                        values.Add(new() { Name = key, Address = address });
+                    }
+                });
+                _postaddresses = values;
+                return _postaddresses; 
+            }
         }
+
+        private static string? _baseapi;
+        private static List<ApiAddress>? _postaddresses;
+        private static readonly object locker = new ();
     }
 }
