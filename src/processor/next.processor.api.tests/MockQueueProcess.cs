8be3cc075dbd97a116b6e29149a303dab01e786c
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2024-08-22 18:48:38 -0500

    feat: adding coverage to base processor

---
+++ src/processor/next.processor.api.tests/MockQueueProcess.cs
@@ -0,0 +1,45 @@
+﻿using next.processor.api.backing;
+using next.processor.api.interfaces;
+using next.processor.api.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace next.processor.api.tests
+{
+    internal class MockQueueProcess : BaseQueueProcess
+    {
+        public MockQueueProcess() : base(GetWrapper())
+        {
+            
+        }
+
+        public void WriteSuccess(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+        public void WriteIterateNext(bool isAllowed)
+        {
+            AllowIterateNext = isAllowed;
+        }
+
+        public override int Index => 1000;
+
+        public override string Name => "Test implementation of queue process";
+
+        public override bool IsSuccess { get; protected set; }
+        public override bool AllowIterateNext { get; protected set; }
+
+        private static MockApiWrapperService GetWrapper()
+        {
+            return new MockApiWrapperService();
+        }
+
+        public override Task<QueueProcessResponses?> ExecuteAsync(QueueProcessResponses? record)
+        {
+            return Task.FromResult(record);
+        }
+    }
+}
---
+++ src/processor/next.processor.api.tests/backing/BaseQueueProcessTests.cs
@@ -0,0 +1,42 @@
+﻿namespace next.processor.api.tests.backing
+{
+    public class BaseQueueProcessTests
+    {
+        [Fact]
+        public void ServiceCanBeCreated()
+        {
+            var error = Record.Exception(() =>
+            {
+                _ = new MockQueueProcess();
+            });
+            Assert.Null(error);
+        }
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ServiceCanToggleSuccessFlag(bool expected)
+        {
+            var error = Record.Exception(() =>
+            {
+                using var sut = new MockQueueProcess();
+                sut.WriteSuccess(expected);
+                Assert.Equal(expected, sut.IsSuccess);
+            });
+            Assert.Null(error);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ServiceCanToggleIterateNextFlag(bool expected)
+        {
+            var error = Record.Exception(() =>
+            {
+                using var sut = new MockQueueProcess();
+                sut.WriteIterateNext(expected);
+                Assert.Equal(expected, sut.AllowIterateNext);
+            });
+            Assert.Null(error);
+        }
+    }
+}
