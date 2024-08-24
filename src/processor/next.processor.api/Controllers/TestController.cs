a91c63ab8e586c93c20dda76152314ac6b20a41c
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2024-08-24 11:48:39 -0500

    test: install firefox to container

--- src/processor/next.processor.api/Controllers/TestController.cs
+++ src/processor/next.processor.api/Controllers/TestController.cs
@@ -1,32 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ICSharpCode.SharpZipLib.BZip2;
+using Microsoft.AspNetCore.Mvc;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using System.Diagnostics;
 using System.Formats.Tar;
+using System.IO;
 using System.IO.Compression;
 using System.Reflection;
 namespace next.processor.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TestController(IWebHostEnvironment hostEnvironment) : ControllerBase
+    public class TestController() : ControllerBase
     {
-        private readonly IWebHostEnvironment _env = hostEnvironment;
-
-
-        [HttpGet("content-path")]
-        public ActionResult ContentPath()
+        [HttpGet("install-browser")]
+        public async Task<ActionResult> BrowserInstallAsync()
         {
-            var contentRootPath = _env.ContentRootPath;
-            var webRootPath = _env.WebRootPath;
+            var environmentDir = Environment.GetEnvironmentVariable("HOME");
+            if (string.IsNullOrEmpty(environmentDir)) { return BadRequest("Environment variable HOME not found"); }
+            var destinationDir = Path.Combine(environmentDir, "util");
+            var mozillaDir = Path.Combine(destinationDir, "mozilla");
+            var paths = new[] { destinationDir, mozillaDir }.ToList();
+            paths.ForEach(path => { if (!Directory.Exists(path)) Directory.CreateDirectory(path); });
+
             var entryAssembly = Assembly.GetEntryAssembly();
             var assemblyPath = entryAssembly == null ? string.Empty : Path.GetDirectoryName(entryAssembly.Location);
-            if (Directory.Exists(assemblyPath))
+
+            if (string.IsNullOrEmpty(assemblyPath) || !Directory.Exists(assemblyPath)) return BadRequest("Application dir is not found.");
+            var files = Directory.GetFiles(assemblyPath, "*.bz2", SearchOption.AllDirectories);
+            var zipfile = files.Length > 0 ? files[0] : string.Empty;
+            if (string.IsNullOrEmpty(zipfile)) return BadRequest("Application zip file is not found.");
+
+            await ExtractBzFileAsync(mozillaDir, zipfile);
+            var currentPaths = Environment.GetEnvironmentVariable("PATH")?.Split(':').ToList() ?? [];
+            var firefoxDir = Path.Combine(destinationDir, "firefox");
+            var firefoxFile = Path.Combine(firefoxDir, "firefox");
+            if (System.IO.File.Exists(firefoxFile) && !currentPaths.Contains(firefoxDir))
             {
-                var files = Directory.GetFiles(assemblyPath, "*.bz2", SearchOption.AllDirectories);
-                var zipfile = files.Length > 0 ? files[0] : string.Empty;
-                return Ok(new { contentRootPath, webRootPath, assemblyPath, zipfile });
+                currentPaths.Add(firefoxDir);
+                Environment.SetEnvironmentVariable("PATH", string.Join(":", currentPaths));
             }
-            return Ok(new { contentRootPath, webRootPath, assemblyPath });
+            return Ok(new { zipfile });
         }
 
         [HttpGet("install-driver")]
@@ -48,7 +62,7 @@ namespace next.processor.api.Controllers
             });
             var downloadPath = Path.Combine(downloadDir, "geckodriver.tar.gz");
             await DownloadGeckoAsync(downloadPath);
-            await SaveGeckoDriverAsync(downloadPath, geckoDir);
+            await ExtractTarFileAsync(downloadPath, geckoDir);
             if (System.IO.File.Exists(geckoFile) && !currentPaths.Contains(geckoDir))
             {
                 currentPaths.Add(geckoDir);
@@ -95,7 +109,7 @@ namespace next.processor.api.Controllers
             using var fileStream = new FileStream(downloadPath, FileMode.Create);
             await responseStream.CopyToAsync(fileStream);
         }
-        private static async Task SaveGeckoDriverAsync(string downloadPath, string outputDirectory, CancellationToken cancellationToken = default)
+        private static async Task ExtractTarFileAsync(string downloadPath, string outputDirectory, CancellationToken cancellationToken = default)
         {
             if (Directory.Exists(outputDirectory) && Directory.GetFiles(outputDirectory, "*.*").Length > 0) return;
             await using var tarStream = new FileStream(downloadPath, new FileStreamOptions { Mode = FileMode.Open, Access = FileAccess.Read, Options = FileOptions.Asynchronous });
@@ -113,6 +127,39 @@ namespace next.processor.api.Controllers
                 cancellationToken: cancellationToken
             );
         }
+        private static async Task ExtractBzFileAsync(string downloadPath, string outputDirectory, CancellationToken cancellationToken = default)
+        {
+            if (Directory.Exists(outputDirectory) && Directory.GetFiles(outputDirectory, "*.*").Length > 0) return;
+            Debug.WriteLine("Extracting bz2 file: {0}", Path.GetFileName(downloadPath));
+            var browserPath = GetFireFoxDownloadUri(_firefoxVersion);
+            using var httpClient = new HttpClient();
+            using var responseStream = await httpClient.GetStreamAsync(browserPath, cancellationToken);
+            await using MemoryStream memoryStream = new();
+            await using (BZip2InputStream gzipStream = new(responseStream))
+            {
+                await gzipStream.CopyToAsync(memoryStream, cancellationToken);
+            }
+            await using (BZip2OutputStream bzipStream = new(new FileStream(downloadPath, FileMode.Create)))
+            {
+                await memoryStream.CopyToAsync(bzipStream, cancellationToken);
+            }
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            await TarFile.ExtractToDirectoryAsync(
+                memoryStream,
+                outputDirectory,
+                overwriteFiles: true,
+                cancellationToken: cancellationToken
+            );
+        }
+
+
+        private static string GetFireFoxDownloadUri(string version)
+        {
+            var address = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/{0}" +
+                "/linux-x86_64/en-US/firefox-{0}.tar.bz2";
+            return string.Format(address, version);
+        }
+        private const string _firefoxVersion = "129.0.2";
         private const string _driverPath = "https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz";
     }
 }
