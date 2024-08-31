9467b8ff9a195f9357a168b057f5c6283642b9db
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2024-08-31 14:56:08 -0500

    fix: set default locations for Home folder

--- src/processor/next.processor.api/utility/EnvironmentHelper.cs
+++ src/processor/next.processor.api/utility/EnvironmentHelper.cs
@@ -4,8 +4,8 @@
     {
         public static string? GetHomeFolder()
         {
-            var home = Environment.GetEnvironmentVariable("HOME");
-            var local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var home = Environment.GetEnvironmentVariable("HOME", EnvironmentVariableTarget.User);
+            var local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
             if (!string.IsNullOrEmpty(home)) return home;
             if (!string.IsNullOrEmpty(local)) return local;
             return null;
